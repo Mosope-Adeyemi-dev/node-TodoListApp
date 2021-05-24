e576ddeb8d936758686ca1b79cc2e7945f7a97b9
Mosope-Adeyemi-dev <78312219+Mosope-Adeyemi-dev@users.noreply.github.com>
2021-05-24 18:00:54 +0100

    implement two types of lists

--- app.js
+++ app.js
@@ -1,11 +1,72 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res)=>{
-    console.log(res.statusCode);
-    res.send("welcome home!!")
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+var itemsArray = ["hello", "it me"];
+let workArray = [];
+var item = "";
+
+app.get("/", (req, res) => {
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    }
+    let date = new Date().toLocaleDateString("en-US", options)
+
+    res.render("list", {
+        listType: date,
+        items: itemsArray
+    });
+});
+
+app.post("/", (req, res) => {
+    if (req.body.submit === "work") {
+        let workItem = req.body.inpt;
+        workArray.push(workItem)
+
+        res.redirect("/work");
+    } else {
+        item = req.body.inpt;
+        itemsArray.push(item);
+
+        res.redirect("/");
+    }
+
+
 })
 
-app.listen(process.env.PORT || 3000, ()=>{
-console.log("server is now running on port 3000");
+app.get("/work", (req, res) => {
+    res.render("list", {
+        listType: "work list",
+        items: workArray
+    });
+})
+
+// app.post("/work", (req, res)=>{
+
+//     let workItem = req.body.inpt;
+//     workArray.push(workItem)
+
+//     app.redirect("/work");
+// })
+
+app.use((req, res) => {
+    res.status(404);
+    res.send("404 - oh uh, Not Found");
+})
+
+// app.use((err, req, res, next)=>{
+//     res.status(500);
+//     res.send("500 - oh uh, Server Error");
+
+// })
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("server is online");
 })
